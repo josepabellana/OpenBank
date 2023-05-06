6e73004db8e2ce08c797b01f6381d980ab5f647c
Josep Abellana Puyol <josep.abe.pu@gmail.com>
2023-05-06 20:50:29 +0200

    Delete client/src/environments directory

--- client/src/environments/environment.dev.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    title: 'Dev Environment Heading',
-    apiURL: 'http://dev.example.com'
-  };
--- client/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    title: 'Production Environment Heading',
-    apiURL: 'https://apiexample.com'
-  };
