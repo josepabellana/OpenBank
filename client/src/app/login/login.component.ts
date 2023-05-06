c62e76c007587b86dc83ff7d12516443a46f3ca2
josepabellana <josep.abe.pu@gmail.com>
2023-05-06 20:27:53 +0200

    x

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from './../../environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  test:string = environment.title;
 }
